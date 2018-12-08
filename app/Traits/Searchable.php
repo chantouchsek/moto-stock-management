66dc32e9bca61cda41a5e4470ef4b8586362f1d3
Touch <s.chantouch@mango-byte.com>
2018-12-08 19:10:55 +0700

    Add uuid for category

--- app/Traits/Searchable.php
+++ app/Traits/Searchable.php
@@ -254,7 +254,6 @@ trait Searchable
      * @param string $column
      * @param string $relevance
      * @param array $words
-     * @param string $compare
      * @param float $relevance_multiplier
      * @param string $pre_word
      * @param string $post_word
--- database/migrations/2018_11_27_232932_create_categories_table.php
+++ database/migrations/2018_11_27_232932_create_categories_table.php
@@ -15,6 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->uuid('uuid');
             $table->string('name')->nullable()->unique();
             $table->string('slug')->nullable()->unique();
             $table->string('description', 255)->nullable();
