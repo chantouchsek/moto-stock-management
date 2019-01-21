4e817c4bcabfa2abc63400615f1a2a6896d659c9
Touch <s.chantouch@mango-byte.com>
2019-01-21 22:17:23 +0700

    fixed

--- app/Http/Requests/Admin/User/UploadAvatarRequest.php
+++ app/Http/Requests/Admin/User/UploadAvatarRequest.php
@@ -24,7 +24,7 @@ class UploadAvatarRequest extends FormRequest
     public function rules()
     {
         return [
-            'avatar' => 'image|mimes:jpeg,png,jpg|max:4056'
+            'avatar' => 'image|mimes:jpeg,png,jpg|max:2048'
         ];
     }
 }
--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -31,7 +31,7 @@ class UserTransformer extends BaseTransformer
             'address' => (string)$item->address,
             'start_work_date' => isset($item->start_work_date) ? $item->start_work_date->toDateString() : '',
             'base_salary' => (float)$item->base_salary,
-            'avatar_url' => (string)$item->hasMedia('avatars') ? config('app.url') . $item->getMedia('avatars')->first()->getUrl('thumb') : 'http://i.pravatar.cc/500?img=' . $item->id,
+            'avatar_url' => (string)$item->hasMedia('avatars') ? config('app.url') . $item->getMedia('avatars')->first()->getUrl() : 'http://i.pravatar.cc/500?img=' . $item->id,
             'status' => (boolean)$item->status,
             'resigned_at' => (string)$item->resigned_at,
             'bonus' => (float)$item->bonus,
