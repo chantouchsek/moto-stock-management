6131bad58f43ce4844c3adab8b9e09bf2f0e3285
Touch <s.chantouch@mango-byte.com>
2019-01-26 15:50:43 +0700

    remove unused function

--- app/Http/Controllers/Admin/Product/ImportController.php
+++ app/Http/Controllers/Admin/Product/ImportController.php
@@ -6,8 +6,6 @@ use App\Http\Requests\Admin\Product\ImportRequest;
 use App\Imports\ProductsImport;
 use App\Traits\Authorizable;
 use App\Http\Controllers\Controller;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
-use Illuminate\Support\Facades\Storage;
 
 class ImportController extends Controller
 {
@@ -25,18 +23,5 @@ class ImportController extends Controller
         $file = $request->file('file');
         (new ProductsImport)->import($file);
         return $this->respondCreated('Products imported success fully');
-    }
-
-    public function downloadSample()
-    {
-        try {
-            $file = Storage::disk('public')->get('/excel/product-upload-sample.xlsx');
-            $headers = [
-                'Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-            ];
-            return $this->respondWithFile($file, 'product-upload-sample.xlsx', $headers);
-        } catch (FileNotFoundException $e) {
-            return $this->respondNotFound('File not found');
-        }
     }
 }
